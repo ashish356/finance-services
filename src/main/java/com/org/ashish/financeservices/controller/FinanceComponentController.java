eac9ecb8e77bd5e99f66f3efc8329818220e5eac
ashish <ashish2008kumar@gmial.com>
2021-04-05 21:18:10 +0100

    added put, delete and exception handling codes

--- src/main/java/com/org/ashish/financeservices/controller/FinanceComponentController.java
+++ src/main/java/com/org/ashish/financeservices/controller/FinanceComponentController.java
@@ -1,12 +1,17 @@
 package com.org.ashish.financeservices.controller;
 
 
+import com.org.ashish.financeservices.exception.ResourceNotFoundException;
 import com.org.ashish.financeservices.model.FinanceComponent;
 import com.org.ashish.financeservices.repository.FinanceComponentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -27,4 +32,36 @@ public class FinanceComponentController {
     {
         return  financeComponentRepository.save(financeComponent);
     }
+
+    @GetMapping("/financeComponents/{id}")
+    public ResponseEntity<FinanceComponent> getFinanceComponentById(@PathVariable(value="id") Long financeComponentId) throws ResourceNotFoundException
+    {
+       FinanceComponent financeComponent=financeComponentRepository.findById(financeComponentId)
+               .orElseThrow(()->new ResourceNotFoundException("Finance Component does not exist for this id : " +financeComponentId));
+
+               return ResponseEntity.ok(financeComponent);
+    }
+    @PutMapping("/financeComponents/{id}")
+    public ResponseEntity<FinanceComponent> updateEmployee(@PathVariable(value = "id") Long financeComponentId, @RequestBody FinanceComponent financeComponentDetails) throws ResourceNotFoundException {
+        FinanceComponent financeComponent = financeComponentRepository.findById(financeComponentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Finance Component not found for this id :: " + financeComponentId));
+        financeComponent.setName(financeComponentDetails.getName());
+
+        final FinanceComponent updatedFinanceComponent = financeComponentRepository.save(financeComponent);
+        return ResponseEntity.ok(updatedFinanceComponent);
+    }
+
+
+    @DeleteMapping("/financeComponents/{id}")
+    public Map<String, Boolean> deleteFinanceComponent(@PathVariable(value = "id") Long financeComponentId)
+            throws ResourceNotFoundException {
+        FinanceComponent financeComponent = financeComponentRepository.findById(financeComponentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Finance Component not found for this id :: " + financeComponentId));
+
+        financeComponentRepository.delete(financeComponent);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
 }
